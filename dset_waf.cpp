0a1505b939f72cd715fe50c8f7eb01a68eb83ac0
rda-dattore <dattore@ucar.edu>
2024-07-26 09:19:23 -0600

    itos -> to_string; other style updates

--- dset_waf.cpp
+++ dset_waf.cpp
@@ -22,15 +22,21 @@
 
 using namespace PostgreSQL;
 using metautils::log_error;
+using metautils::log_warning;
 using std::cerr;
+using std::cout;
+using std::deque;
 using std::endl;
 using std::regex;
 using std::regex_search;
 using std::string;
 using std::stringstream;
+using std::to_string;
 using std::unique_ptr;
 using std::unordered_set;
 using std::vector;
+using strutils::append;
+using strutils::split;
 using strutils::strand;
 using unixutils::mysystem2;
 using xercesc_3_2::XercesDOMParser;
@@ -41,15 +47,15 @@ using xercesc_3_2::XMLString;
 
 metautils::Directives metautils::directives;
 metautils::Args metautils::args;
-string myerror="";
-string mywarning="";
+string myerror = "";
+string mywarning = "";
 
-string user=getenv("USER");
+const string USER = getenv("USER");
 
 struct LocalArgs {
   LocalArgs() : git_repos(),queued_only(false),all_non_public(false) {}
 
-  std::vector<string> git_repos;
+  vector<string> git_repos;
   bool queued_only,all_non_public;
 } local_args;
 struct TimerThreadStruct {
@@ -77,7 +83,7 @@ public:
     if (!parse_error.empty()) {
       parse_error+="\n";
     }
-    parse_error+="Warning ("+strutils::itos(e.getLineNumber())+":"+strutils::itos(e.getColumnNumber())+"): "+string(msg);
+    parse_error+="Warning ("+to_string(e.getLineNumber())+":"+to_string(e.getColumnNumber())+"): "+string(msg);
     XMLString::release(&msg);
   }
   void error(const SAXParseException& e) {
@@ -85,7 +91,7 @@ public:
     if (!parse_error.empty()) {
       parse_error+="\n";
     }
-    parse_error+="Error ("+strutils::itos(e.getLineNumber())+":"+strutils::itos(e.getColumnNumber())+"): "+string(msg);
+    parse_error+="Error ("+to_string(e.getLineNumber())+":"+to_string(e.getColumnNumber())+"): "+string(msg);
     XMLString::release(&msg);
   }
   void fatalError(const SAXParseException& e) {
@@ -93,7 +99,7 @@ public:
     if (!parse_error.empty()) {
       parse_error+="\n";
     }
-    parse_error+="Fatal error ("+strutils::itos(e.getLineNumber())+":"+strutils::itos(e.getColumnNumber())+"): "+string(msg);
+    parse_error+="Fatal error ("+to_string(e.getLineNumber())+":"+to_string(e.getColumnNumber())+"): "+string(msg);
     XMLString::release(&msg);
   }
   void resetErrors() {
@@ -106,7 +112,7 @@ extern "C" void segv_handler(int) {
   stringstream oss,ess;
   mysystem2("/bin/tcsh -c \"/gpfs/u/home/dattore/bin/vm/dset_waf DBRESET\"",
       oss, ess);
-  std::cout << "Database was reset for the next update" << endl;
+  cout << "Database was reset for the next update" << endl;
 }
 
 
@@ -164,7 +170,7 @@ t->parse_error = "";
   return nullptr;
 }
 
-void do_push(const std::deque<string>& arg_list) {
+void do_push(const deque<string>& arg_list) {
   Server server(metautils::directives.database_server, "metadata", "metadata",
       "rdadb");
 
@@ -181,7 +187,7 @@ void do_push(const std::deque<string>& arg_list) {
     q.set("dsid", "search.datasets", "type = 'W'");
     if (q.submit(server) != 0) {
       log_error("Database error while getting work-in-progress datasets: '" +
-          q.error() + "'", "dset_waf", user);
+          q.error() + "'", "dset_waf", USER);
       exit(1);
     }
     for (const auto& r : q) {
@@ -190,7 +196,7 @@ void do_push(const std::deque<string>& arg_list) {
     q.set("dsid", "metautil.dset_waf", "uflag = ''");
     if (q.submit(server) != 0) {
       log_error("Database error while getting queued datasets: '" + q.error() +
-          "'", "dset_waf", user);
+          "'", "dset_waf", USER);
       exit(1);
     }
     for (const auto& r : q) {
@@ -262,7 +268,7 @@ void do_push(const std::deque<string>& arg_list) {
     for (const auto& dsid : dsids) {
       if (server.update("metautil.dset_waf", "uflag = '" + uflag + "'", "dsid "
           "= '" + dsid + "'") < 0) {
-        log_error("flag set failed for flag '" + uflag + "'", "dset_waf", user);
+        log_error("flag set failed for flag '" + uflag + "'", "dset_waf", USER);
       }
     }
   }
@@ -281,7 +287,7 @@ void do_push(const std::deque<string>& arg_list) {
     ofs.open(fname.c_str());
     if (!ofs.is_open()) {
       log_error("Error opening output file '" + fname + "'; uflag was '" + uflag
-          + "'", "dset_waf", user);
+          + "'", "dset_waf", USER);
     }
     ofs << "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>" << endl;
     auto date_stamp_index = metadata_record.str().find("<gmd:dateStamp>");
@@ -315,10 +321,10 @@ void do_push(const std::deque<string>& arg_list) {
       pthread_cancel(tts.tid);
     }
     if (tts.timed_out) {
-      log_error("parser timed out for flag '" + uflag + "'", "dset_waf", user);
+      log_error("parser timed out for flag '" + uflag + "'", "dset_waf", USER);
     } else if (!vts.parse_error.empty()) {
       log_error(dsid + " - XML validation failed; error(s) follow:\n" + vts.
-          parse_error + "\nuflag was '" + uflag + "'", "dset_waf", user);
+          parse_error + "\nuflag was '" + uflag + "'", "dset_waf", USER);
     }
   }
   static const string REPO_HEAD = "/data/ptmp/dattore/git-repos";
@@ -331,11 +337,11 @@ void do_push(const std::deque<string>& arg_list) {
     //   may need to do a 'git stash drop "stash@{0}"'?
     if (!ess.str().empty()) {
       if (exit_status == 0) {
-        metautils::log_warning("git pull message: '" + ess.str() + "'; uflag "
-            "was '" + uflag + "'", "dset_waf", user);
+        log_warning("git pull message: '" + ess.str() + "'; uflag was '" +
+            uflag + "'", "dset_waf", USER);
       } else {
         log_error("git pull error: '" + ess.str() + "'; uflag was '" + uflag +
-            "'", "dset_waf", user);
+            "'", "dset_waf", USER);
       }
     }
     size_t num_added = 0;
@@ -349,7 +355,7 @@ void do_push(const std::deque<string>& arg_list) {
             + dsid + ".xml\"", oss, ess);
         if (!ess.str().empty()) {
           log_error("git add error for " + dsid + ": '" + ess.str() + "'; "
-              "uflag was '" + uflag + "'", "dset_waf", user);
+              "uflag was '" + uflag + "'", "dset_waf", USER);
         } else {
           ++num_added;
         }
@@ -361,13 +367,13 @@ void do_push(const std::deque<string>& arg_list) {
           "-m 'auto update' -a\"", oss, ess);
       if (!ess.str().empty()) {
         log_error("git commit error: '" + ess.str() + "'; uflag was '" + uflag +
-            "'", "dset_waf", user);
+            "'", "dset_waf", USER);
       } else {
         mysystem2("/bin/tcsh -c \"cd " + REPO_HEAD + "/" + repo + "; git push "
             "-q\"", oss, ess);
         if (!ess.str().empty() && ess.str().find("remote: Resolving deltas") != 0) {
           log_error("git push error: '" + ess.str() + "'; uflag was '" + uflag +
-              "'", "dset_waf", user);
+              "'", "dset_waf", USER);
         }
       }
     }
@@ -375,7 +381,7 @@ void do_push(const std::deque<string>& arg_list) {
   if (!uflag.empty()) {
     if (server._delete("metautil.dset_waf", "uflag = '" + uflag + "'") < 0) {
       log_error("unable to remove queued datasets with flag '" + uflag + "'",
-          "dset_waf", user);
+          "dset_waf", USER);
     }
   }
   server.disconnect();
@@ -388,27 +394,26 @@ void do_push(const std::deque<string>& arg_list) {
   }
 }
 
-void do_delete(const std::deque<string>& arg_list)
-{
-  std::vector<string> dsids;
+void do_delete(const deque<string>& arg_list) {
+  vector<string> dsids;
   if (local_args.all_non_public) {
-    Server server(metautils::directives.database_server,"metadata","metadata","rdadb");
-    LocalQuery query("dsid","search.datasets","type not in ('P', 'H')");
+    Server server(metautils::directives.database_server, "metadata", "metadata",
+        "rdadb");
+    LocalQuery query("dsid", "search.datasets", "type not in ('P', 'H')");
     if (query.submit(server) < 0) {
-      log_error("Database error while getting non-public datasets: '"+query.error()+"'","dset_waf",user);
+      log_error("Database error while getting non-public datasets: '" + query.
+          error() + "'", "dset_waf", USER);
       exit(1);
     }
     for (const auto& row : query) {
       dsids.emplace_back(row[0]);
     }
     server.disconnect();
-  }
-  else {
+  } else {
     if (arg_list.front() == "all") {
       cerr << "Error: datasets can only be deleted by explicit naming" << endl;
       exit(1);
-    }
-    else {
+    } else {
       for (const auto& arg : arg_list) {
         if (arg != "all") {
           dsids.emplace_back(arg);
@@ -416,39 +421,44 @@ void do_delete(const std::deque<string>& arg_list)
       }
     }
   }
-  static const string REPO_HEAD="/data/ptmp/dattore/git-repos";
+  static const string REPO_HEAD = "/data/ptmp/dattore/git-repos";
   for (const auto& repo : local_args.git_repos) {
     string repo_tail;
-    auto idx=repo.find("/");
+    auto idx = repo.find("/");
     if (idx != string::npos) {
-      repo_tail=repo.substr(idx+1)+"/";
+      repo_tail = repo.substr(idx+1) + "/";
     }
-    size_t num_deleted=0;
+    size_t num_deleted = 0;
     for (const auto& dsid : dsids) {
-      stringstream oss,ess;
-      mysystem2("/bin/tcsh -c \"cd "+REPO_HEAD+"/"+repo+"; git rm ds"+dsid+".xml\"",oss,ess);
+      stringstream oss, ess;
+      mysystem2("/bin/tcsh -c \"cd " + REPO_HEAD + "/" + repo + "; git rm ds" +
+          dsid + ".xml\"", oss, ess);
       if (!ess.str().empty()) {
-        if (ess.str() != "fatal: pathspec 'ds"+dsid+".xml' did not match any files\n") {
-          metautils::log_warning("git rm error for "+dsid+": '"+ess.str()+"'","dset_waf",user);
+        if (ess.str() != "fatal: pathspec 'ds"+dsid+".xml' did not match any "
+            "files\n") {
+          log_warning("git rm error for " + dsid + ": '" + ess.str() + "'",
+              "dset_waf", USER);
         }
-      }
-      else {
+      } else {
         ++num_deleted;
       }
     }
     if (num_deleted > 0) {
-      stringstream oss,ess;
-      mysystem2("/bin/tcsh -c \"cd "+REPO_HEAD+"/"+repo+"; git pull -q\"",oss,ess);
+      stringstream oss, ess;
+      mysystem2("/bin/tcsh -c \"cd " + REPO_HEAD + "/" + repo + "; git pull "
+          "-q\"", oss, ess);
       if (!ess.str().empty()) {
-        log_error("git pull error: '"+ess.str()+"'","dset_waf",user);
+        log_error("git pull error: '" + ess.str() + "'", "dset_waf", USER);
       } else {
-        mysystem2("/bin/tcsh -c \"cd "+REPO_HEAD+"/"+repo+"; git commit -m 'auto remove' -a\"",oss,ess);
+        mysystem2("/bin/tcsh -c \"cd " + REPO_HEAD + "/" + repo + "; git "
+            "commit -m 'auto remove' -a\"", oss, ess);
         if (!ess.str().empty()) {
-          log_error("git commit error: '"+ess.str()+"'","dset_waf",user);
+          log_error("git commit error: '" + ess.str() + "'", "dset_waf", USER);
         } else {
-          mysystem2("/bin/tcsh -c \"cd "+REPO_HEAD+"/"+repo+"; git push -q\"",oss,ess);
+          mysystem2("/bin/tcsh -c \"cd " + REPO_HEAD + "/" + repo + "; git "
+              "push -q\"", oss, ess);
           if (!ess.str().empty()) {
-            log_error("git push error: '"+ess.str()+"'","dset_waf",user);
+            log_error("git push error: '" + ess.str() + "'", "dset_waf", USER);
           }
         }
       }
@@ -456,38 +466,38 @@ void do_delete(const std::deque<string>& arg_list)
   }
 }
 
-void do_db_reset(const std::deque<string>& arg_list)
-{
-  Server server(metautils::directives.database_server,"metadata","metadata","rdadb");
-  LocalQuery query("distinct dsid","metautil.dset_waf");
+void do_db_reset(const deque<string>& arg_list) {
+  Server server(metautils::directives.database_server, "metadata", "metadata",
+      "rdadb");
+  LocalQuery query("distinct dsid", "metautil.dset_waf");
   if (query.submit(server) < 0) {
-    log_error("Database error while trying to fix a failed push: '"+query.error()+"'","dset_waf",user);
+    log_error("Database error while trying to fix a failed push: '" + query.
+        error() + "'", "dset_waf", USER);
     exit(1);
   }
   if (server._delete("metautil.dset_waf") < 0) {
-    log_error("Database error while trying to clear a failed push: '"+server.error()+"'","dset_waf",user);
+    log_error("Database error while trying to clear a failed push: '" + server.
+        error() + "'", "dset_waf", USER);
     exit(1);
   }
-  auto status=0;
-  stringstream dslist;
+  auto status = 0;
+  string dslist;
   for (const auto& row : query) {
-    if (!dslist.str().empty()) {
-      dslist << ", ";
-    }
-    dslist << row[0];
-    if (server.insert("metautil.dset_waf","'"+row[0]+"',''") < 0) {
-      status=-1;
+    append(dslist, row[0], ", ");
+    if (server.insert("metautil.dset_waf", "'" + row[0] + "', ''") < 0) {
+      status = -1;
     }
   }
   if (status < 0) {
-    log_error("Database error while trying to clear a failed push - dataset list: '"+dslist.str()+"'","dset_waf",user);
+    log_error("Database error while trying to clear a failed push - dataset "
+        "list: '" + dslist + "'", "dset_waf", USER);
     exit(1);
   }
 }
 
 int main(int argc, char **argv) {
   if (argc < 3 && (argc != 2 || string(argv[1]) != "DBRESET")) {
-    cerr << "usage: dset_waf <action> [options...] <dslist>" << endl;
+    cerr << "usage: dset_waf <action> [options...] <dsid_list>" << endl;
     cerr << "\nrequired:" << endl;
     cerr << "<action>  must be one of:" << endl;
     cerr << "            PUSH - add/update dataset(s)" << endl;
@@ -495,7 +505,7 @@ int main(int argc, char **argv) {
     cerr << "            DBRESET - reset the database after a failed push" << endl;
     cerr << "<dslist>  must be one of:" << endl;
     cerr << "            \"all\" for all datasets" << endl;
-    cerr << "            \"nnn.n ...\" one or more individual dataset numbers" << endl;
+    cerr << "            \"dnnnnnn ...\" one or more individual dataset numbers" << endl;
     cerr << "\noptions:" << endl;
     cerr << "-R <repo>  restrict to <repo>, otherwise by default, operate on all known repos" << endl;
     cerr << "\nPUSH options:" << endl;
@@ -504,28 +514,26 @@ int main(int argc, char **argv) {
     cerr << "--all-non-public  identify and delete all non-public datasets" << endl;
     exit(1);
   }
-  metautils::args.args_string=unixutils::unix_args_string(argc,argv,'%');
-  auto arg_list=strutils::split(metautils::args.args_string,"%");
-  auto action=arg_list.front();
+  metautils::args.args_string = unixutils::unix_args_string(argc, argv, '%');
+  auto arg_list = split(metautils::args.args_string, "%");
+  auto action = arg_list.front();
   arg_list.pop_front();
-  while (arg_list.size() > 0) {
+  while (!arg_list.empty()) {
     if (arg_list.front()[0] == '-') {
       if (arg_list.front()[1] == 'R') {
         arg_list.pop_front();
         local_args.git_repos.emplace_back(arg_list.front());
-      }
-      else if (arg_list.front() == "--queued-only") {
-        local_args.queued_only=true;
-      }
-      else if (arg_list.front() == "--all-non-public") {
-        local_args.all_non_public=true;
+      } else if (arg_list.front() == "--queued-only") {
+        local_args.queued_only = true;
+      } else if (arg_list.front() == "--all-non-public") {
+        local_args.all_non_public = true;
       }
       arg_list.pop_front();
     } else {
       break;
     }
   }
-  if (action != "DBRESET" && arg_list.size() == 0) {
+  if (action != "DBRESET" && arg_list.empty()) {
     cerr << "Error: no dataset(s) specified" << endl;
     exit(1);
   }
@@ -534,17 +542,14 @@ int main(int argc, char **argv) {
     local_args.git_repos.emplace_back("dash-rda-prod/RDA-Datasets");
   }
 //  signal(SIGSEGV,segv_handler);
-  metautils::read_config("dset_waf","","");
+  metautils::read_config("dset_waf", "", "");
   if (action == "PUSH") {
     do_push(arg_list);
-  }
-  else if (action == "DELETE") {
+  } else if (action == "DELETE") {
     do_delete(arg_list);
-  }
-  else if (action == "DBRESET") {
+  } else if (action == "DBRESET") {
     do_db_reset(arg_list);
-  }
-  else {
+  } else {
     cerr << "Error: invalid action" << endl;
     exit(1);
   }
